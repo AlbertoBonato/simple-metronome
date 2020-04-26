ceba4bb053d85d8728462135341edaa8e7831306
yyi4644 <alberto.bonato@infocamere.it>
2020-04-26 14:12:20 +0200

    Initial commit

---
+++ app/src/main/java/it/aljava/simplemetronome/MainActivity.kt
@@ -0,0 +1,106 @@
+package it.aljava.simplemetronome
+
+import android.annotation.SuppressLint
+import android.media.MediaPlayer
+import android.media.PlaybackParams
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import it.aljava.simplemetronome.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainActivityViewModel
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var metronome: MediaPlayer
+    private val tracks: List<Uri> = listOf(
+        Uri.parse("android.resource://it.aljava.simplemetronome/" + R.raw.loop_40_bpm),
+        Uri.parse("android.resource://it.aljava.simplemetronome/" + R.raw.loop_120_bpm))
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        metronome = MediaPlayer.create(this, tracks.elementAt(1))
+        metronome.isLooping = true
+        metronome.setOnErrorListener(object :MediaPlayer.OnErrorListener{
+            override fun onError(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
+                Log.e("MainActivity" , "Metronome error what $what" +
+                        ", extra $extra")
+                return true
+            }
+        })
+        viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mainActivityViewModel = viewModel
+        binding.setLifecycleOwner(this)
+        // observer start-stop
+        viewModel.metronomeOn.observe(this, Observer {
+            if (it == true) {
+                binding.playButton.setText(R.string.play_button_stop)
+                binding.playButton.setBackgroundResource(R.color.red)
+                if (!metronome.isPlaying)
+                    metronome.start()
+            } else if (it == false) {
+                binding.playButton.setText(R.string.play_button_play)
+                binding.playButton.setBackgroundResource(R.color.green)
+                if (metronome.isPlaying)
+                    metronome.pause()
+            }
+        })
+        // observer metronome params changed
+        viewModel.metronomeParams.observe(this, Observer {
+            if (it.first == null) {
+                // do nothing
+            } else if (!it.first!!) {
+                updateSpeed()
+            } else if (it.first!!) {
+                updateSpeedAndTrack(it.second)
+            }
+        })
+    }
+
+    private fun updateSpeed() {
+        val preventRestart = !metronome.isPlaying
+        val newPlaybackParams = PlaybackParams()
+        newPlaybackParams.setSpeed(viewModel.speedRate.value!!)
+        metronome.playbackParams = newPlaybackParams
+        if (preventRestart) {
+            metronome.pause()
+            viewModel.switchOffMetronome()
+        }
+    }
+
+    private fun updateSpeedAndTrack(trackId: Int) {
+        val restart = this.metronome.isPlaying
+        restart.let {
+            if (it) metronome.pause()
+            metronome.stop()
+        }
+        metronome.release()
+        metronome = MediaPlayer()
+        metronome.setDataSource(this, tracks.elementAt(trackId))
+        val newPlaybackParams = PlaybackParams()
+        viewModel.speedRate.value?.let { it1 -> newPlaybackParams.setSpeed(it1) }
+        metronome.playbackParams = newPlaybackParams
+        metronome.prepare()
+        restart.let {
+            if (it) metronome.start()
+        }
+    }
+
+    override fun onPause() {
+        metronome.pause()
+        viewModel.switchOffMetronome()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        metronome.stop()
+        metronome.release()
+    }
+}
---
+++ app/src/main/java/it/aljava/simplemetronome/MainActivityViewModel.kt
@@ -0,0 +1,131 @@
+package it.aljava.simplemetronome
+
+import android.util.Log
+import android.widget.SeekBar
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+
+    private val initialBpm = 120
+    private val bpmProgressOffset = 20
+    private val changeTrackBpmThreshold = 90
+
+    private val _speedRate = MutableLiveData<Float>()
+    val speedRate: LiveData<Float>
+        get() = _speedRate
+
+    private val _progress = MutableLiveData<Int>()
+    val progress: LiveData<Int>
+        get() = _progress
+
+    private val bpm = MutableLiveData<Int>()
+
+    private val _metronomeOn = MutableLiveData<Boolean>()
+    val metronomeOn: LiveData<Boolean>
+        get() = _metronomeOn
+    /*
+        0 --> R.raw.loop_40_bpm
+        1 --> R.raw.loop_120_bpm
+     */
+    private val _trackSelected = MutableLiveData<Int>()
+
+    /*
+    Updated when at least one between speedRate and trackSelected changes
+    metronomeParams.first = false   -> only speedRate changes
+    metronomeParams.first = true    -> both speedRate and trackSelected changes
+    metronomeParams.second = id of track selected (0 o 1)
+    Initialization value: (null, 0)
+     */
+    private val _metronomeParams = MutableLiveData<Pair<Boolean?, Int>>()
+    val metronomeParams: LiveData<Pair<Boolean?, Int>>
+        get() = _metronomeParams
+
+    private val _playButtonText = MutableLiveData<String>()
+    val playButtonText: LiveData<String>
+        get() = _playButtonText
+
+    val bpmString = MutableLiveData<String>()
+
+    init {
+        bpm.value = initialBpm
+        bpmString.value = initialBpm.toString()
+        _progress.value = initialBpm.minus(bpmProgressOffset)
+        _metronomeOn.value = false
+        _trackSelected.value = 1
+        _speedRate.value = getSpeedRateFromBpmAndTrack(bpm.value!!, _trackSelected.value!!)
+        _metronomeParams.value = Pair(null, _trackSelected.value!!)
+        _playButtonText.value = "Play"
+    }
+
+    fun switchOffMetronome() {
+        _metronomeOn.value = false
+    }
+
+    fun incrementBpm() {
+        if (bpm.value!! < 300) {
+            bpm.value = bpm.value?.inc()
+            bpmString.value = bpm.value.toString()
+            _progress.value = _progress.value?.inc()
+            _speedRate.value = getSpeedRateFromBpmAndTrack(bpm.value!!, _trackSelected.value!!)
+        }
+        val changeTrack = bpm.value == changeTrackBpmThreshold
+        _metronomeParams.value = Pair(changeTrack, _trackSelected.value!!)
+    }
+
+    fun decrementBpm() {
+        if (bpm.value!! > 20) {
+            bpm.value = bpm.value?.dec()
+            bpmString.value = bpm.value.toString()
+            _progress.value = _progress.value?.dec()
+            _speedRate.value = getSpeedRateFromBpmAndTrack(bpm.value!!, _trackSelected.value!!)
+        }
+        val changeTrack = bpm.value == (changeTrackBpmThreshold - 1)
+        _metronomeParams.value = Pair(changeTrack, _trackSelected.value!!)
+    }
+
+    fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+        bpmString.value = bpmFromProgress(progress).toString()
+    }
+
+    fun onStopTrackingTouch(seek: SeekBar) {
+        val newBpm = bpmFromProgress(seek.progress)
+        val tc = trackChanged(newBpm, bpm.value!!)
+        _trackSelected.value = checkTrackToPlay(newBpm)
+        Log.i("AAAA_VM", "trackChanged $tc")
+        _progress.value = seek.progress
+        bpm.value = newBpm
+        bpmString.value = newBpm.toString()
+        _speedRate.value = getSpeedRateFromBpmAndTrack(newBpm, _trackSelected.value!!)
+        _metronomeParams.value = Pair(tc, _trackSelected.value!!)
+    }
+
+    fun togglePlayback() {
+        _metronomeOn.value = _metronomeOn.value?.not() ?: false
+    }
+
+    private fun trackChanged(newBpm: Int, oldBpm: Int): Boolean {
+        if (newBpm < changeTrackBpmThreshold
+            && oldBpm >= changeTrackBpmThreshold) {
+            return true
+        } else return newBpm >= changeTrackBpmThreshold
+                && oldBpm < changeTrackBpmThreshold
+    }
+
+    private fun checkTrackToPlay(bpm: Int): Int {
+        return if (bpm >= changeTrackBpmThreshold) 1
+        else 0
+    }
+
+    private fun getSpeedRateFromBpmAndTrack(bpm: Int, track: Int): Float {
+        return if (track == 0) {
+            bpm.div(this.initialBpm.toFloat())?.times(3)
+        } else {
+            bpm.div(this.initialBpm.toFloat())
+        }
+    }
+
+    private fun bpmFromProgress(progress: Int) = progress.plus(bpmProgressOffset)
+
+}
